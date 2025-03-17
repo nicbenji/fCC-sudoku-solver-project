86d944bd151655a022ecfa3428b3aa6a0913d724
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-17 21:18:16 +0100

    feat: check row function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -7,8 +7,38 @@ class SudokuSolver {
     return /^[1-9.]+$/.test(puzzleString);
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, _column, value) {
+    // e.g. puzzleString = see ./puzzle-strings.js row = A-I column = 1-9 value = 1-9
+    const rowString = this.getRow(puzzleString, row);
 
+    // NOTE: Optionally check if column already filled
+
+    const rowNums = rowString.match(/\d/g);
+    return !rowNums.map((numString) => Number(numString)).includes(value);
+  }
+
+  getRow(puzzleString, rowLetter) {
+    const rowMapper = {
+      A: 1,
+      B: 2,
+      C: 3,
+      D: 4,
+      E: 5,
+      F: 6,
+      G: 7,
+      H: 8,
+      I: 9
+    }
+    const rowNum = rowMapper[rowLetter.toUpperCase()];
+    if (!rowNum) {
+      throw new Error('invalid row');
+    }
+
+    const LETTERS_PER_ROW = 9;
+    const end = rowNum * LETTERS_PER_ROW;
+    const start = end - LETTERS_PER_ROW;
+    const row = puzzleString.slice(start, end);
+    return row;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
@@ -25,4 +55,3 @@ class SudokuSolver {
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -26,4 +26,20 @@ suite('Unit Tests', () => {
     assert.isFalse(solver.validate(puzzle));
   });
 
+  test('should handle a valid row placement', () => {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    const row = 'a';
+    const column = 1;
+    const value = 7;
+    assert.isTrue(solver.checkRowPlacement(puzzle, row, column, value));
+  });
+
+  test('should handle an invalid row placement', () => {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    const row = 'a';
+    const column = 1;
+    const value = 1;
+    assert.isFalse(solver.checkRowPlacement(puzzle, row, column, value));
+  });
+
 });
